2c12b3d5242134ed8d3fc0cea96b16066b09b816
Ludovic Picard <luldopic@googlemail.com>
2022-03-15 23:01:50 +0000

    Update livecoinapi.py

--- livecoinapi.py
+++ livecoinapi.py
@@ -10,14 +10,13 @@ import time
 
 
 class coin_data:
-    def __init__(self, coin_list):
-        self.code_list = coin_list
+    def __init__(self):
         self.headers = {
             'content-type': 'application/json',
             'x-api-key': 'e5d88ba2-cbea-43e5-8dd8-9b9e7d0bdc05'
         }
 
-    def get_historical_single(self, currency, coin, date, delta):
+    def get_historical_single(self, coin, date, delta=1, currency="USD"):
         url = "https://api.livecoinwatch.com/coins/single/history"
         payload = json.dumps(
             {
@@ -31,8 +30,7 @@ class coin_data:
 
         return json.loads(response.text)
 
-    def get_current_single(self,coin):
-
+    def get_current_single(self, coin):
         url = "https://api.livecoinwatch.com/coins/single"
 
         payload = json.dumps({
@@ -45,6 +43,20 @@ class coin_data:
         dict = json.loads(response.text)
         dict["date"] = self.convertUNIX(datetime.now())
         return dict
+
+    def get_coin_list(self):
+        url = "https://api.livecoinwatch.com/coins/list"
+
+        payload = json.dumps({
+            "currency": "USD",
+            "sort": "rank",
+            "order": "ascending",
+            "offset": 0,
+            "limit": 100,
+            "meta": False
+        })
+        response = requests.request("POST", url, headers=self.headers, data=payload)
+        return json.loads(response.text)
 
     def convertUNIX(self, date):
         date = int(time.mktime(date.timetuple())) * 1000
