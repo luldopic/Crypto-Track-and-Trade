a97c28d8dd2f7340dcf8ecabef0ce7b6c9650979
Ludovic Picard <luldopic@googlemail.com>
2022-03-15 21:55:26 +0000

    Update CryptoSQL.py

--- CryptoSQL.py
+++ CryptoSQL.py
@@ -1,26 +1,54 @@
 import mysql.connector
 
 
-class CryptoDB:
+class DB:
     def __init__(self):
-        self.db = self.connectToDB
+        self.connectToDB
+
+    def addCoin(self, coin_name):
+        if not self.coinTableExists(coin_name):
+            SQL = "CREATE TABLE {coin_name} " \
+                  "(date DATETIME NOT NULL, " \
+                  "rate BIGINT NOT NULL, " \
+                  "volume DOUBLE(13), " \
+                  "cap DOUBLE (13), " \
+                  "liquidity DOUBLE(13)," \
+                  "PRIMARY KEY (date)".format(coin_name=coin_name)
+            self.executeSQLCursor(SQL)
+        else:
+            print("Coin already added")
+
+    def addRecord(self, record):
+        columns = str(tuple(record.keys())).replace("'", "")
+        values = str(tuple(record.values())).replace("\"", "'")
+        SQL = "INSERT INTO {table} {columns} VALUES {values}".format(table=table_name, columns=columns, values=values)
+        self.executeSQLCursor(SQL)
+        self.db.commit()
+
+    def coinTableExists(self,coin_name):
+        SQL = "SHOW TABLES LIKE '{name}'".format(name=table_name)
+        res = self.executeSQLCursor(SQL)
+        if len(res) == 0:
+            return False
+        else:
+            return True
+
+    def executeSQLCursor(self, command):
+        cursor = self.db.cursor()
+        cursor.reset()
+        cursor.execute(command)
+        res = cursor.fetchall()
+        cursor.close()
+        return res
 
     def connectToDB(self):
         with open("MySQLcredentials.json") as handler:
             try:
                 credentials = json.load(handler)
-                if db_exist:
-                    self.db = mysql.connector.connect(host=credentials["host"], user=credentials["user"],
-                                                      password=credentials["password"], database="GmailMail")
-                else:
-                    self.db = mysql.connector.connect(host=credentials["host"], user=credentials["user"],
-                                                      password=credentials["password"])
+                self.db = mysql.connector.connect(host=credentials["host"], user=credentials["user"],
+                                                  password=credentials["password"], database="cryptotrack")
                 print("Successful Connection")
             except Exception as e:
-                error = str(e)
-                if error.__contains__("Unknown database"):
-                    raise UnknownDatabase("Database cannot be found")
-                else:
-                    print(e)
+                print(e)
             finally:
                 del credentials
