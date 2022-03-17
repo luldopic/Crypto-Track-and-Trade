ae21934d40e0c3e4acf5b114904e92b7cfb70bf8
Ludovic Picard <luldopic@googlemail.com>
2022-03-17 18:37:36 +0000

    Update Crypto_DataScience.py

--- Crypto_DataScience.py
+++ Crypto_DataScience.py
@@ -1,3 +1,41 @@
 import CryptoSQL
+import addRecords
+import time
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 
-DB = CryptoSQL.CryptoDB()
+class Predict1hour:
+    def __init__(self):
+        self.DB = CryptoSQL.CryptoDB()
+        record = addRecords.updateDatabase()
+        record.updateEntryCount()
+        self.coin_list = record.get_coin_list()
+
+    def retrieveRecordByUnix(self, coin, date):
+        unixdate = int(time.mktime(date.timetuple())) * 1000
+        SQL = "SELECT * FROM {coin} WHERE unixdate = {unixdate}".format(coin=coin, unixdate=unixdate)
+        res = self.DB.executeSQLCursor(SQL)
+        record = res[0]
+        return record
+
+    def retrieveAllRecord(self, coin):
+        SQL = "SELECT * FROM {coin}".format(coin=coin)
+        res = self.DB.executeSQLCursor(SQL)
+        arr = np.array(res)
+        arr = np.transpose(arr)
+        return arr
+
+test = Predict1hour()
+testdate = datetime(2021, 1, 1, 0, 0, 2)
+record = test.retrieveAllRecord("BTC")
+arr = np.array(record)
+arr = np.transpose(arr)
+lab = ["Date","Rate", "Volume", "Cap", "Liquidity"]
+x = 1
+y = 3
+plt.scatter(arr[x],arr[y])
+plt.xlabel(lab[x])
+plt.ylabel(lab[y])
+plt.show()
+pass
